70d9460a35cad2c097a9cc9a4693e19a1aec855d
Liam <liam@opentensor.dev>
2024-08-15 20:02:09 +0200

    pull timestamps from clickhouse

--- scraper_service/shared/block_metadata.py
+++ scraper_service/shared/block_metadata.py
@@ -1,4 +1,48 @@
+from shared.clickhouse.utils import get_clickhouse_client
 from shared.substrate import get_substrate_client
+
+timestamps = dict()
+
+
+def refresh_timestamp_dict(n):
+    """
+    Caches n -> n+10k timestamps
+    """
+    clickhouse = get_clickhouse_client()
+    timestamps.clear()
+
+    # Fetch 10k timestamps at a time
+    query = f"""
+        SELECT timestamp, block_number
+        FROM shovel_block_timestamps
+        WHERE block_number >= {n} AND block_number < {n + 10_000}
+    """
+    r = clickhouse.execute(query)
+    for (timestamp, block_number) in r:
+        timestamps[block_number] = timestamp
+
+
+def get_block_timestamp(n, block_hash):
+    """
+    First tries to fetch from cache, then chain.
+    """
+    if n not in timestamps:
+        refresh_timestamp_dict(n)
+
+    if n in timestamps:
+        substrate = get_substrate_client()
+        return int(timestamps[n].timestamp())
+    else:
+        print("WARN: Block n timestamp not found in Clickhouse, falling back to chain")
+        substrate = get_substrate_client()
+        return int(
+            substrate.query(
+                "Timestamp",
+                "Now",
+                block_hash=block_hash,
+            ).serialize()
+            / 1000
+        )
 
 
 def get_block_metadata(n):
@@ -10,13 +54,8 @@ def get_block_metadata(n):
 
     substrate = get_substrate_client()
     block_hash = substrate.get_block_hash(n)
-    block_timestamp = int(
-        substrate.query(
-            "Timestamp",
-            "Now",
-            block_hash=block_hash,
-        ).serialize()
-        / 1000
-    )
+
+    # If still not there, just get it from the chain
+    block_timestamp = get_block_timestamp(n, block_hash)
 
     return (block_timestamp, block_hash)
