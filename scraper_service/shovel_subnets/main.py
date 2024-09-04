6dc1def0a4c01c556c8c0ce72fffa52ff3ba474b
Liam <liam@opentensor.dev>
2024-09-04 09:05:31 +0200

    wip shovel subnets

--- scraper_service/shovel_subnets/main.py
+++ scraper_service/shovel_subnets/main.py
@@ -1,4 +1,6 @@
 from shared.block_metadata import get_block_metadata
+from tqdm import tqdm
+from datetime import datetime
 from shared.clickhouse.batch_insert import buffer_insert
 from shared.shovel_base_class import ShovelBaseClass
 from shared.substrate import create_storage_key_cached, get_substrate_client
@@ -42,12 +44,6 @@ def create_table():
             axon_placeholder1 UInt8 CODEC(Delta, ZSTD),
             axon_placeholder2 UInt8 CODEC(Delta, ZSTD),
 
-            prometheus_block UInt64 CODEC(Delta, ZSTD),
-            prometheus_version UInt32 CODEC(Delta, ZSTD),
-            prometheus_ip UInt128 CODEC(Delta, ZSTD),
-            prometheus_port UInt16 CODEC(Delta, ZSTD),
-            prometheus_ip_type UInt8 CODEC(Delta, ZSTD),
-
             rank UInt16 CODEC(Delta, ZSTD),
             emission UInt64 CODEC(Delta, ZSTD),
             incentive UInt16 CODEC(Delta, ZSTD),
@@ -55,14 +51,97 @@ def create_table():
             trust UInt16 CODEC(Delta, ZSTD),
             validator_trust UInt16 CODEC(Delta, ZSTD),
             dividends UInt16 CODEC(Delta, ZSTD),
+            stake UInt64 CODEC(Delta, ZSTD),
+            weights Array((u16, u16)) CODEC(Delta, ZSTD),
             last_update UInt64 CODEC(Delta, ZSTD),
 
             validator_permit Bool CODEC(Delta, ZSTD),
             pruning_score UInt16 CODEC(Delta, ZSTD),
         ) ENGINE = ReplacingMergeTree()
+        PARTITION BY toYYYYMM(timestamp)
         ORDER BY (subnet_id, neuron_id, timestamp)
         """
         get_clickhouse_client().execute(query)
+
+
+cache = {}
+
+
+def batch(iterable, n=1):
+    length = len(iterable)
+    for i in range(0, length, n):
+        yield iterable[i:i + n]
+
+
+def get_coldkeys_and_stakes(hotkeys, timestamp, block_hash):
+    global cache
+
+    need_to_query = []
+    for hotkey in hotkeys:
+        if (hotkey, timestamp) not in cache:
+            need_to_query.append(hotkey)
+
+    if len(need_to_query) > 0:
+        clickhouse = get_clickhouse_client()
+        batch_size = 1000
+        responses = []
+        dt_object = datetime.fromtimestamp(timestamp)
+        formatted_date = dt_object.strftime("%Y-%m-%d %H:%M:%S")
+        print(
+            f"Quering {len(need_to_query) /
+                       batch_size} batches of coldkeys and stakes"
+        )
+        for hotkey_batch in tqdm(batch(need_to_query, batch_size)):
+            hotkeys_list = "', '".join(hotkey_batch)
+            hotkeys_str = f"('{hotkeys_list}')"
+            query = f"""
+                    SELECT
+                        timestamp,
+                        hotkey,
+                        coldkey,
+                        stake
+                    FROM test_db.shovel_hotkey_owner_map AS o
+                    INNER JOIN test_db.shovel_stake_double_map AS s
+                    ON o.timestamp = s.timestamp AND o.coldkey = s.coldkey AND o.hotkey = s.hotkey
+                    WHERE hotkey IN {hotkeys_str}
+                    AND timestamp >= '{formatted_date}' AND timestamp < addHours('{formatted_date}', 1)
+                """
+            r = clickhouse.execute(query)
+            responses.extend(r)
+        for response in responses:
+            cache[(int(response[0].timestamp()), response[1])] = (
+                response[2], response[3]
+            )
+
+    coldkeys_and_stakes = dict()
+    for hotkey in hotkeys:
+        # handle a hotkey without a stake
+        if (timestamp, hotkey) not in cache:
+            coldkey = get_substrate_client().query(
+                "SubtensorModule",
+                "Owner",
+                block_hash=block_hash,
+                params=[hotkey],
+            ).value
+            stake = get_substrate_client().query(
+                "SubtensorModule",
+                "Stake",
+                block_hash=block_hash,
+                params=[hotkey, coldkey],
+            )
+            if stake != 0:
+                print(
+                    f"ERROR: Hotkey {hotkey} has stake {
+                        stake} but not in Clickhouse"
+                )
+                exit(0)
+            cache[(timestamp, hotkey)] = (coldkey, stake)
+
+        # we should have stakes for all hotkeys now!
+        coldkey, stake = cache[(timestamp, hotkey)]
+        coldkeys_and_stakes[hotkey] = (coldkey, stake)
+
+    return coldkeys_and_stakes
 
 
 def do_process_block(n):
@@ -70,254 +149,19 @@ def do_process_block(n):
     substrate = get_substrate_client()
     # Create table if it doesn't exist
 
-    (block_timestamp, block_hash) = get_block_metadata(3_500_000)
+    (block_timestamp, block_hash) = get_block_metadata(1_400_000)
 
     start = time.time()
-    rust = rust_bindings.query(block_hash)
+    (neurons, hotkeys) = rust_bindings.query_neuron_info(block_hash)
     print("rust took ", time.time() - start)
-    start = time.time()
-    py = substrate.query_map(
-        "SubtensorModule", "Stake",
-        page_size=1000,
-        block_hash=block_hash
+
+    coldkeys_and_stakes = get_coldkeys_and_stakes(
+        hotkeys, block_timestamp, block_hash
     )
-    for _ in py:
-        pass
-    print("py took ", time.time() - start)
+
     exit(1)
 
-    start = time.time()
-    active = substrate.query_map(
-        "SubtensorModule", "Active",
-        page_size=1000,
-        block_hash=block_hash
-    )
-    for _ in active:
-        pass
-    rank = substrate.query_map(
-        "SubtensorModule", "Rank",
-        page_size=1000,
-        block_hash=block_hash
-    )
-    for _ in rank:
-        pass
-    trust = substrate.query_map(
-        "SubtensorModule", "Trust",
-        page_size=1000,
-        block_hash=block_hash
-    )
-    for _ in trust:
-        pass
-    emission = substrate.query_map(
-        "SubtensorModule", "Emission",
-        page_size=1000,
-        block_hash=block_hash
-    )
-    for _ in emission:
-        pass
-    consensus = substrate.query_map(
-        "SubtensorModule", "Consensus",
-        page_size=1000,
-        block_hash=block_hash
-    )
-    for _ in consensus:
-        pass
-    incentive = substrate.query_map(
-        "SubtensorModule", "Incentive",
-        page_size=1000,
-        block_hash=block_hash
-    )
-    for _ in incentive:
-        pass
-    dividends = substrate.query_map(
-        "SubtensorModule", "Dividends",
-        page_size=1000,
-        block_hash=block_hash
-    )
-    for _ in dividends:
-        pass
-    last_update = substrate.query_map(
-        "SubtensorModule", "LastUpdate",
-        page_size=1000,
-        block_hash=block_hash
-    )
-    for _ in last_update:
-        pass
-    pruning_scores = substrate.query_map(
-        "SubtensorModule", "PruningScores",
-        page_size=1000,
-        block_hash=block_hash
-    )
-    for _ in pruning_scores:
-        pass
-    validator_trust = substrate.query_map(
-        "SubtensorModule", "ValidatorTrust",
-        page_size=1000,
-        block_hash=block_hash
-    )
-    for _ in validator_trust:
-        pass
-    validator_permit = substrate.query_map(
-        "SubtensorModule", "ValidatorPermit",
-        page_size=1000,
-        block_hash=block_hash
-    )
-    for _ in validator_permit:
-        pass
-    axons = substrate.query_map(
-        "SubtensorModule", "Axons",
-        page_size=1000,
-        block_hash=block_hash
-    )
-    for _ in axons:
-        pass
-    prometheus = substrate.query_map(
-        "SubtensorModule", "Prometheus",
-        page_size=1000,
-        block_hash=block_hash
-    )
-    for _ in prometheus:
-        pass
-
-    end = time.time()
-    print(f"Took {end - start} seconds")
-
-    # for ((hotkey, coldkey), value) in stakes:
-    #     buffer_insert("shovel_stake_double_map", [n, block_timestamp, f"'{
-    #                   hotkey}'", f"'{coldkey}'", value.value])
-
-# def do_process_block(n):
-#     substrate = get_substrate_client()
-#     # Create table if it doesn't exist
-#
-#     (block_timestamp, block_hash) = get_block_metadata(3_500_000)
-#
-#     subnets = []
-#     result = substrate.query_map(
-#         'SubtensorModule', 'NetworksAdded',
-#         block_hash=block_hash,
-#         page_size=1000
-#     )
-#     for r in result:
-#         print(r[0].value)
-#         subnets.append(r[0].value)
-#
-#     start = time.time()
-#     coldkeys_keys = []
-#     axon_keys = []
-#     prometheus_keys = []
-#     start = time.time()
-#
-#     ac
-#
-#     for subnet_id in subnets:
-#         active_vec_key = create_storage_key_cached(
-#             "SubtensorModule", "Active", (subnet_id,)
-#         )
-#         rank_vec_key = create_storage_key_cached(
-#             "SubtensorModule", "Rank", (subnet_id,)
-#         )
-#         trust_vec_key = create_storage_key_cached(
-#             "SubtensorModule", "Trust", (subnet_id,)
-#         )
-#         emission_vec_key = create_storage_key_cached(
-#             "SubtensorModule", "Emission", (subnet_id,)
-#         )
-#         consensus_vec_key = create_storage_key_cached(
-#             "SubtensorModule", "Consensus", (subnet_id,)
-#         )
-#         incentive_vec_key = create_storage_key_cached(
-#             "SubtensorModule", "Incentive", (subnet_id,)
-#         )
-#         dividends_vec_key = create_storage_key_cached(
-#             "SubtensorModule", "Dividends", (subnet_id,)
-#         )
-#         last_update_vec_key = create_storage_key_cached(
-#             "SubtensorModule", "LastUpdate", (subnet_id,)
-#         )
-#         pruning_scores_vec_key = create_storage_key_cached(
-#             "SubtensorModule", "PruningScores", (subnet_id,)
-#         )
-#         validator_trust_vec_key = create_storage_key_cached(
-#             "SubtensorModule", "ValidatorTrust", (subnet_id,)
-#         )
-#         validator_permit_vec_key = create_storage_key_cached(
-#             "SubtensorModule", "ValidatorPermit", (subnet_id,)
-#         )
-#         n_neurons_key = create_storage_key_cached(
-#             "SubtensorModule", "SubnetworkN", (subnet_id,)
-#         )
-#
-#         start = time.time()
-#         active_vec, rank_vec, trust_vec, emission_vec, consensus_vec, incentive_vec, dividends_vec, last_update_vec, pruning_scores_vec, validator_trust_vec, validator_permit_vec, n_neurons = substrate.query_multi(
-#             [
-#                 active_vec_key,
-#                 rank_vec_key,
-#                 trust_vec_key,
-#                 emission_vec_key,
-#                 consensus_vec_key,
-#                 incentive_vec_key,
-#                 dividends_vec_key,
-#                 last_update_vec_key,
-#                 pruning_scores_vec_key,
-#                 validator_trust_vec_key,
-#                 validator_permit_vec_key,
-#                 n_neurons_key,
-#             ],
-#             block_hash=block_hash,
-#         )
-#         n_neurons = n_neurons[1].value
-#         start = time.time()
-#
-#         hotkeys_keys = [
-#             create_storage_key_cached(
-#                 "SubtensorModule", "Keys", (subnet_id, neuron_id)
-#             ) for neuron_id in range(n_neurons)
-#         ]
-#         hotkeys = substrate.query_multi(
-#             hotkeys_keys, block_hash
-#         )
-#         hotkeys = map(lambda x: x[1].value, hotkeys)
-#
-#         _coldkeys_keys = [
-#             create_storage_key_cached(
-#                 "SubtensorModule", "Owner", (hotkey,)
-#             )
-#             for hotkey in hotkeys
-#         ]
-#
-#         _axon_keys = [
-#             create_storage_key_cached(
-#                 "SubtensorModule", "Axons", (subnet_id, hotkey)
-#             )
-#             for hotkey in hotkeys
-#         ]
-#
-#         _prometheus_keys = [
-#             create_storage_key_cached(
-#                 "SubtensorModule", "Prometheus", (subnet_id, hotkey))
-#             for hotkey in hotkeys
-#         ]
-#         coldkeys_keys.extend(_coldkeys_keys)
-#         prometheus_keys.extend(_prometheus_keys)
-#         axon_keys.extend(_axon_keys)
-#         # r = substrate.query_multi(
-#         #     coldkeys_keys + axon_keys + prometheus_keys,
-#         #     block_hash=block_hash,
-#         # )
-#
-#         end = time.time()
-#         print(f"Took {end - start} seconds")
-#
-#     r = substrate.query_multi(
-#         coldkeys_keys + axon_keys + prometheus_keys,
-#         block_hash=block_hash,
-#     )
-#     print(f"all done in {time.time() - start}")
-#
-#     # for ((hotkey, coldkey), value) in stakes:
-#     #     buffer_insert("shovel_stake_double_map", [n, block_timestamp, f"'{
-#     #                   hotkey}'", f"'{coldkey}'", value.value])
+    # todo: get axon info
 
 
 def main():
--- scraper_service/shovel_subnets/rust_bindings/src/lib.rs
+++ scraper_service/shovel_subnets/rust_bindings/src/lib.rs
@@ -1,6 +1,8 @@
-use std::sync::Arc;
-
+use deadpool::unmanaged;
+use futures::future::join_all;
+use parity_scale_codec::Decode;
 use pyo3::prelude::*;
+use std::{collections::HashMap, sync::Arc};
 use subxt::{
     backend::{legacy::LegacyBackend, rpc::RpcClient},
     utils::H256,
@@ -11,50 +13,303 @@ use tokio;
 #[subxt::subxt(runtime_metadata_path = "./metadata.scale")]
 pub mod subtensor {}
 
-/// Formats the sum of two numbers as string.
-async fn query_inner(block_hash: String) -> PyResult<Vec<(String, u64)>> {
-    let start = std::time::Instant::now();
-    let url = std::env::var("SUBSTRATE_ARCHIVE_NODE_URL").unwrap();
+#[pyclass]
+#[derive(Debug, Default)]
+#[allow(unused)]
+struct NeuronInfo {
+    subnet_id: u16,
+    neuron_id: u16,
 
-    let client = RpcClient::from_insecure_url(url)
+    block_hash: String,
+    hotkey: String,
+    pub active: bool,
+
+    pub rank: u16,
+    pub emission: u64,
+    pub incentive: u16,
+    pub consensus: u16,
+    pub trust: u16,
+    pub validator_trust: u16,
+    pub dividends: u16,
+    pub weights: Vec<(u16, u16)>,
+
+    pub last_update: u64,
+    pub pruning_scores: u16,
+}
+
+macro_rules! fetch_and_process_kvs {
+    ($api_pool:expr, $block_hash:expr, $query:expr, $label:expr) => {{
+        async {
+            let api = $api_pool.get().await.unwrap();
+            let start = std::time::Instant::now();
+            let mut iter = api.storage().at($block_hash).iter($query).await.unwrap();
+            let mut kvs = Vec::new();
+
+            while let Some(Ok(kv)) = iter.next().await {
+                kvs.push((kv.key_bytes, kv.value));
+            }
+            let end = std::time::Instant::now();
+            println!("{} Time elapsed: {:?}", $label, end - start);
+
+            kvs
+        }
+    }};
+}
+
+async fn get_api() -> OnlineClient<PolkadotConfig> {
+    let url = std::env::var("SUBSTRATE_ARCHIVE_NODE_URL").unwrap();
+    let client = RpcClient::from_insecure_url(url.clone())
         .await
         .expect("Failed to connect to node");
     let backend = LegacyBackend::<PolkadotConfig>::builder()
         .storage_page_size(1000)
         .build(client);
-    let api = OnlineClient::from_backend(Arc::new(backend)).await.unwrap();
+    OnlineClient::from_backend(Arc::new(backend)).await.unwrap()
+}
 
-    let stake_query = subtensor::storage().subtensor_module().stake_iter();
+/// Formats the sum of two numbers as string.
+async fn query_neuron_info_inner(block_hash: String) -> PyResult<(Vec<NeuronInfo>, Vec<String>)> {
+    let start = std::time::Instant::now();
     let block_hash = hex::decode(block_hash.trim_start_matches("0x")).expect("Decoding failed");
     let block_hash = H256::from_slice(&block_hash);
 
-    let mut iter = api
-        .storage()
-        .at(block_hash)
-        .iter(stake_query)
-        .await
-        .unwrap();
+    let keys_query = subtensor::storage().subtensor_module().keys_iter();
+    let active_query = subtensor::storage().subtensor_module().active_iter();
+    let rank_query = subtensor::storage().subtensor_module().rank_iter();
+    let trust_query = subtensor::storage().subtensor_module().trust_iter();
+    let emission_query = subtensor::storage().subtensor_module().emission_iter();
+    let consensus_query = subtensor::storage().subtensor_module().consensus_iter();
+    let incentive_query = subtensor::storage().subtensor_module().incentive_iter();
+    let dividends_query = subtensor::storage().subtensor_module().dividends_iter();
+    let last_update_query = subtensor::storage().subtensor_module().last_update_iter();
+    let pruning_scores_query = subtensor::storage()
+        .subtensor_module()
+        .pruning_scores_iter();
+    let validator_trust_query = subtensor::storage()
+        .subtensor_module()
+        .validator_trust_iter();
+    let weights_query = subtensor::storage().subtensor_module().weights_iter();
 
-    let mut kvs = vec![];
-    while let Some(Ok(kv)) = iter.next().await {
-        let key = hex::encode(&kv.key_bytes);
-        kvs.push((key, kv.value));
-    }
+    // not important
+    // let bonds_query = subtensor::storage().subtensor_module().bonds_iter();
+    // let stake_query = subtensor::storage().subtensor_module().stake_iter();
+    // let prometheus_query = subtensor::storage().subtensor_module().prometheus_iter();
+
+    // axons are scraped seperately (too slow for every block)
+    // let axons_query = subtensor::storage().subtensor_module().axons_iter();
+
+    let concurrency = 32;
+    let api_futures = (0..concurrency).map(|_| get_api()).collect::<Vec<_>>();
+    let api_pool = join_all(api_futures).await.into_iter().collect::<Vec<_>>();
+    let api_pool = unmanaged::Pool::from(api_pool);
+
+    let (
+        keys_kvs,
+        active_kvs,
+        rank_kvs,
+        trust_kvs,
+        emission_kvs,
+        consensus_kvs,
+        incentive_kvs,
+        dividends_kvs,
+        last_update_kvs,
+        pruning_scores_kvs,
+        validator_trust_kvs,
+        weights_kvs,
+    ) = tokio::join!(
+        fetch_and_process_kvs!(&api_pool, block_hash, keys_query, "keys"),
+        fetch_and_process_kvs!(&api_pool, block_hash, active_query, "active"),
+        fetch_and_process_kvs!(&api_pool, block_hash, rank_query, "rank"),
+        fetch_and_process_kvs!(&api_pool, block_hash, trust_query, "trust"),
+        fetch_and_process_kvs!(&api_pool, block_hash, emission_query, "emission"),
+        fetch_and_process_kvs!(&api_pool, block_hash, consensus_query, "consensus"),
+        fetch_and_process_kvs!(&api_pool, block_hash, incentive_query, "incentive"),
+        fetch_and_process_kvs!(&api_pool, block_hash, dividends_query, "dividends"),
+        fetch_and_process_kvs!(&api_pool, block_hash, last_update_query, "last_update"),
+        fetch_and_process_kvs!(
+            &api_pool,
+            block_hash,
+            pruning_scores_query,
+            "pruning_scores"
+        ),
+        fetch_and_process_kvs!(
+            &api_pool,
+            block_hash,
+            validator_trust_query,
+            "validator_trust"
+        ),
+        fetch_and_process_kvs!(&api_pool, block_hash, weights_query, "weights"),
+    );
     let end = std::time::Instant::now();
-    println!("Query took: {:?}", end - start);
-    Ok(kvs)
+    println!("Get main stuff elapsed: {:?}", end - start);
+
+    let mut neuron_map: HashMap<(u16, u16), NeuronInfo> = HashMap::new();
+    let mut hotkeys = Vec::new();
+
+    // Init neuron map with hotkeys
+    for (key, value) in keys_kvs {
+        let mut second_last_two_bytes = &key[key.len() - 4..key.len() - 2];
+        let mut last_two_bytes = &key[key.len() - 2..];
+        let subnet_id = u16::decode(&mut second_last_two_bytes).unwrap();
+        let neuron_id = u16::decode(&mut last_two_bytes).unwrap();
+        let hotkey = value.to_string();
+        hotkeys.push(hotkey.clone());
+        let block_hash = block_hash.clone().to_string();
+        let neuron_info = NeuronInfo {
+            subnet_id,
+            neuron_id,
+            hotkey,
+            block_hash,
+            ..Default::default()
+        };
+        neuron_map.insert((subnet_id, neuron_id), neuron_info);
+    }
+
+    // Set active
+    for (key, value) in active_kvs {
+        let mut last_two_bytes = &key[key.len() - 2..];
+        let subnet_id = u16::decode(&mut last_two_bytes).unwrap();
+        for (neuron_id, active) in value.iter().enumerate() {
+            neuron_map
+                .get_mut(&(subnet_id, neuron_id as u16))
+                .expect("Subnet neuron not initialized!")
+                .active = *active;
+        }
+    }
+
+    // Set rank
+    for (key, value) in rank_kvs {
+        let mut last_two_bytes = &key[key.len() - 2..];
+        let subnet_id = u16::decode(&mut last_two_bytes).unwrap();
+        for (neuron_id, rank) in value.iter().enumerate() {
+            neuron_map
+                .get_mut(&(subnet_id, neuron_id as u16))
+                .expect("Subnet neuron not initialized!")
+                .rank = *rank;
+        }
+    }
+
+    // Set trust
+    for (key, value) in trust_kvs {
+        let mut last_two_bytes = &key[key.len() - 2..];
+        let subnet_id = u16::decode(&mut last_two_bytes).unwrap();
+        for (neuron_id, trust) in value.iter().enumerate() {
+            neuron_map
+                .get_mut(&(subnet_id, neuron_id as u16))
+                .expect("Subnet neuron not initialized!")
+                .trust = *trust;
+        }
+    }
+
+    // Set emission
+    for (key, value) in emission_kvs {
+        let mut last_two_bytes = &key[key.len() - 2..];
+        let subnet_id = u16::decode(&mut last_two_bytes).unwrap();
+        for (neuron_id, emission) in value.iter().enumerate() {
+            neuron_map
+                .get_mut(&(subnet_id, neuron_id as u16))
+                .expect("Subnet neuron not initialized!")
+                .emission = *emission;
+        }
+    }
+
+    // Set consensus
+    for (key, value) in consensus_kvs {
+        let mut last_two_bytes = &key[key.len() - 2..];
+        let subnet_id = u16::decode(&mut last_two_bytes).unwrap();
+        for (neuron_id, consensus) in value.iter().enumerate() {
+            neuron_map
+                .get_mut(&(subnet_id, neuron_id as u16))
+                .expect("Subnet neuron not initialized!")
+                .consensus = *consensus;
+        }
+    }
+
+    // Set incentive
+    for (key, value) in incentive_kvs {
+        let mut last_two_bytes = &key[key.len() - 2..];
+        let subnet_id = u16::decode(&mut last_two_bytes).unwrap();
+        for (neuron_id, incentive) in value.iter().enumerate() {
+            neuron_map
+                .get_mut(&(subnet_id, neuron_id as u16))
+                .expect("Subnet neuron not initialized!")
+                .incentive = *incentive;
+        }
+    }
+
+    // Set dividends
+    for (key, value) in dividends_kvs {
+        let mut last_two_bytes = &key[key.len() - 2..];
+        let subnet_id = u16::decode(&mut last_two_bytes).unwrap();
+        for (neuron_id, dividends) in value.iter().enumerate() {
+            neuron_map
+                .get_mut(&(subnet_id, neuron_id as u16))
+                .expect("Subnet neuron not initialized!")
+                .dividends = *dividends;
+        }
+    }
+
+    // Set last_update
+    for (key, value) in last_update_kvs {
+        let mut last_two_bytes = &key[key.len() - 2..];
+        let subnet_id = u16::decode(&mut last_two_bytes).unwrap();
+        for (neuron_id, last_update) in value.iter().enumerate() {
+            neuron_map
+                .get_mut(&(subnet_id, neuron_id as u16))
+                .expect("Subnet neuron not initialized!")
+                .last_update = *last_update;
+        }
+    }
+
+    // Set pruning_scores
+    for (key, value) in pruning_scores_kvs {
+        let mut last_two_bytes = &key[key.len() - 2..];
+        let subnet_id = u16::decode(&mut last_two_bytes).unwrap();
+        for (neuron_id, pruning_scores) in value.iter().enumerate() {
+            neuron_map
+                .get_mut(&(subnet_id, neuron_id as u16))
+                .expect("Subnet neuron not initialized!")
+                .pruning_scores = *pruning_scores;
+        }
+    }
+
+    // Set validator_trust
+    for (key, value) in validator_trust_kvs {
+        let mut last_two_bytes = &key[key.len() - 2..];
+        let subnet_id = u16::decode(&mut last_two_bytes).unwrap();
+        for (neuron_id, validator_trust) in value.iter().enumerate() {
+            neuron_map
+                .get_mut(&(subnet_id, neuron_id as u16))
+                .expect("Subnet neuron not initialized!")
+                .validator_trust = *validator_trust;
+        }
+    }
+
+    // Set Weights
+    for (key, weights) in weights_kvs {
+        let mut second_last_two_bytes = &key[key.len() - 4..key.len() - 2];
+        let mut last_two_bytes = &key[key.len() - 2..];
+        let subnet_id = u16::decode(&mut second_last_two_bytes).unwrap();
+        let neuron_id = u16::decode(&mut last_two_bytes).unwrap();
+        neuron_map
+            .get_mut(&(subnet_id, neuron_id as u16))
+            .expect("Subnet neuron not initialized!")
+            .weights = weights.to_owned();
+    }
+
+    Ok((neuron_map.into_values().collect(), hotkeys))
 }
 
 #[pyfunction]
-fn query(block_hash: String) -> PyResult<Vec<(String, u64)>> {
+fn query_neuron_info(block_hash: String) -> PyResult<(Vec<NeuronInfo>, Vec<String>)> {
     tokio::runtime::Runtime::new()
         .unwrap()
-        .block_on(query_inner(block_hash))
+        .block_on(query_neuron_info_inner(block_hash))
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn rust_bindings(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(query, m)?)?;
+    m.add_function(wrap_pyfunction!(query_neuron_info, m)?)?;
     Ok(())
 }
