63965109b3bc93968db56d4872e31befec5cd99d
hey Michal! <michal@opentensor.dev>
2024-09-10 18:43:14 +0200

    working daily stake rust bindings

--- scraper_service/shovel_daily_stake/main.py
+++ scraper_service/shovel_daily_stake/main.py
@@ -14,140 +14,18 @@ import rust_bindings
 from tqdm import tqdm
 from functools import lru_cache
 
-
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s %(process)d %(message)s")
 
-last_stakes_proof = None
-
-STAKES_PREFIX = "0x658faa385070e074c85bf6b568cf055522fbe0bd0cb77b6b6f365f641b0de381"
-
-
-def check_root_read_proof(block_hash):
-    global last_stakes_proof
-
-    substrate = get_substrate_client()
-    r = substrate.rpc_request(
-        "state_getReadProof",
-        params=[[STAKES_PREFIX], block_hash]
-    )
-    this_stakes_proof = set(r["result"]["proof"])
-    stake_map_changed = last_stakes_proof is None or last_stakes_proof.isdisjoint(
-        this_stakes_proof
-    )
-    last_stakes_proof = this_stakes_proof
-
-    return stake_map_changed
-
-
-class StakeDoubleMapShovel(ShovelBaseClass):
-    def __init__(self, name):
-        super().__init__(name, skip_interval=3200)
-
-    table_name = "shovel_stake_daily_map"
-
-    def process_block(self, n):
-        do_process_block(n, self.table_name)
-
-
-prev_pending_emissions = {}
-
-stake_map = dict()
-
-
-def do_process_block(n, table_name):
-    substrate = get_substrate_client()
-    # Create table if it doesn't exist
-    if not table_exists(table_name):
-        query = f"""
-        CREATE TABLE IF NOT EXISTS {table_name} (
-            block_number UInt64 CODEC(Delta, ZSTD),
-            timestamp DateTime CODEC(Delta, ZSTD),
-            hotkey String CODEC(ZSTD),
-            coldkey String CODEC(ZSTD),
-            stake UInt64 CODEC(Delta, ZSTD)
-        ) ENGINE = ReplacingMergeTree()
-        PARTITION BY toYYYYMM(timestamp)
-        ORDER BY (coldkey, hotkey, timestamp)
-        """
-        get_clickhouse_client().execute(query)
-
-
-    (block_timestamp, block_hash) = get_block_metadata(n)
-
-    hotkeys_needing_update = set()
-
-    # Get pending emission amount for every subnet
-    result = rust_bindings.query_map_pending_emission(block_hash)
-    for subnet_id, pending_emission in result:
-        if (subnet_id not in prev_pending_emissions) or pending_emission == 0 and prev_pending_emissions[subnet_id] != 0:
-            print(
-                f"Refreshing all hotkeys from subnet {
-                    subnet_id} due to update..."
-            )
-            subnet_hotkeys = rust_bindings.query_subnet_hotkeys(
-                block_hash, subnet_id
-            )
-
-            count = 0
-            for (neuron_id, hotkey) in subnet_hotkeys:
-                hotkeys_needing_update.add(hotkey)
-                count += 1
-            print(
-                f"Found {count} hotkeys for {subnet_id}"
-            )
-
-        prev_pending_emissions[subnet_id] = pending_emission
-
-    # Check if we're up to date
-    events_synced_block_query = "SELECT block_number FROM test_db.shovel_checkpoints FINAL WHERE shovel_name = 'events';"
-    events_synced_block = get_clickhouse_client().execute(
-        events_synced_block_query)[0][0]
-    hotkey_owner_map_synced_block_query = "SELECT block_number FROM test_db.shovel_checkpoints FINAL WHERE shovel_name = 'hotkey_owner_map';"
-    hotkey_owner_map_synced_block = get_clickhouse_client().execute(
-        hotkey_owner_map_synced_block_query)[0][0]
-
-    while (events_synced_block < n or hotkey_owner_map_synced_block < n):
-        print("Waiting for events and hotkey_owner_map tables to sync...")
-        time.sleep(60)
-        events_synced_block = get_clickhouse_client().execute(
-            events_synced_block_query)[0][0]
-        hotkey_owner_map_synced_block = get_clickhouse_client().execute(
-            hotkey_owner_map_synced_block_query)[0][0]
-
-    # Get hotkeys with stake events this block
-    dt_object = datetime.fromtimestamp(block_timestamp)
-    formatted_date = dt_object.strftime("%Y-%m-%d %H:%M:%S")
-    distinct_hotkeys_query = f"""
-        SELECT DISTINCT(hotkey) from test_db.agg_stake_events WHERE timestamp = '{formatted_date}'
-    """
-    distinct_hotkeys = get_clickhouse_client().execute(distinct_hotkeys_query)
-
-    for r in distinct_hotkeys:
-        hotkeys_needing_update.add(r[0])
-
-    # Get agg stake events for this block
-    r = rust_bindings.query_hotkeys_stakes(
-        block_hash, list(hotkeys_needing_update)
-    )
-    for (hotkey, coldkey_stakes) in r:
-        for (coldkey, stake) in coldkey_stakes:
-            stake_map[(hotkey, coldkey)] = stake
-
-    for ((hotkey, coldkey), stake) in stake_map.items():
-        buffer_insert(
-            table_name,
-            [n, block_timestamp, f"'{hotkey}'", f"'{coldkey}'", stake]
-        )
-
-
-@lru_cache
-def create_storage_key_cached(pallet, storage, args):
-    return get_substrate_client().create_storage_key(pallet, storage, list(args))
-
 
 def main():
-    StakeDoubleMapShovel(name="stake_daily_map").start()
+    # StakeDoubleMapShovel(name="stake_daily_map").start()
+    (block_timestamp, block_hash) = get_block_metadata(123123)
+    results = rust_bindings.query_block_stakes(block_hash)
+    for result in results:
+        hotkey = result[0]
+        coldkey, amount = result[1][0]
+        print(f"BLOCK: {block_hash}, HOTKEY: {hotkey}, COLDKEY: {coldkey}, AMOUNT: {amount}")
 
 
 if __name__ == "__main__":
--- scraper_service/shovel_daily_stake/rust_bindings/src/lib.rs
+++ scraper_service/shovel_daily_stake/rust_bindings/src/lib.rs
@@ -1,8 +1,4 @@
-use deadpool::unmanaged;
-use futures::future::join_all;
-use parity_scale_codec::Decode;
 use pyo3::prelude::*;
-use std::str::FromStr;
 use std::sync::Arc;
 use subxt::{
     backend::{legacy::LegacyBackend, rpc::RpcClient},
@@ -24,120 +20,44 @@ async fn get_api() -> OnlineClient<PolkadotConfig> {
     OnlineClient::from_backend(Arc::new(backend)).await.unwrap()
 }
 
-async fn query_map_pending_emission_inner(block_hash: String) -> PyResult<Vec<(u16, u64)>> {
-    let api = get_api().await;
-    let block_hash = hex::decode(block_hash.trim_start_matches("0x")).expect("Decoding failed");
-    let block_hash = H256::from_slice(&block_hash);
-    let query = subtensor::storage()
-        .subtensor_module()
-        .pending_emission_iter();
-    let mut iter = api.storage().at(block_hash).iter(query).await.unwrap();
-    let mut kvs = Vec::new();
-    while let Some(Ok(kv)) = iter.next().await {
-        let mut last_two_bytes = &kv.key_bytes[kv.key_bytes.len() - 2..];
-        let subnet_id = u16::decode(&mut last_two_bytes).unwrap();
-        kvs.push((subnet_id, kv.value));
-    }
-    Ok(kvs)
-}
-
-async fn query_subnet_hotkeys_inner(
+async fn query_block_stakes_inner(
     block_hash: String,
-    subnet_id: u16,
-) -> PyResult<Vec<(u16, String)>> {
+) -> PyResult<Vec<(String, Vec<(String, u64)>)>> {
     let api = get_api().await;
     let block_hash = hex::decode(block_hash.trim_start_matches("0x")).expect("Decoding failed");
     let block_hash = H256::from_slice(&block_hash);
-    let query = subtensor::storage()
-        .subtensor_module()
-        .keys_iter1(subnet_id);
-    let mut iter = api.storage().at(block_hash).iter(query).await.unwrap();
-    let mut kvs = Vec::new();
-    while let Some(Ok(kv)) = iter.next().await {
-        let mut last_two_bytes = &kv.key_bytes[kv.key_bytes.len() - 2..];
-        let neuron_id = u16::decode(&mut last_two_bytes).unwrap();
-        kvs.push((neuron_id, kv.value.to_string()));
-    }
-    Ok(kvs)
-}
-
-async fn query_hotkey_stakes_inner(
-    api: &OnlineClient<PolkadotConfig>,
-    block_hash: &H256,
-    hotkey: &String,
-) -> Vec<(String, u64)> {
-    let hotkey = AccountId32::from_str(hotkey.as_str()).expect("Invalid hotkey");
-    let query = subtensor::storage().subtensor_module().stake_iter1(hotkey);
+    let query = subtensor::storage().subtensor_module().stake_iter();
     let mut iter = api
         .storage()
         .at(block_hash.clone())
         .iter(query)
         .await
         .unwrap();
-    let mut kvs = Vec::new();
+    let mut kvs: Vec<(String, Vec<(String, u64)>)> = Vec::new();
+
     while let Some(Ok(kv)) = iter.next().await {
-        let last_32_bytes: [u8; 32] = kv.key_bytes[kv.key_bytes.len() - 32..].try_into().unwrap();
-        let coldkey = AccountId32::from(last_32_bytes).to_string();
-        kvs.push((coldkey, kv.value));
+        let coldkey_bytes: [u8; 32] = kv.key_bytes[kv.key_bytes.len() - 32..].try_into().unwrap();
+        let hotkey_bytes: [u8; 32] = kv.key_bytes[kv.key_bytes.len() - 64..kv.key_bytes.len() - 32].try_into().unwrap();
+        let coldkey = AccountId32::from(coldkey_bytes).to_string();
+        let hotkey = AccountId32::from(hotkey_bytes).to_string();
+        kvs.push((coldkey, vec![(hotkey, kv.value)]));
     }
-    kvs
+
+    Ok(kvs)
 }
 
-async fn query_hotkeys_stakes_inner(
+#[pyfunction]
+fn query_block_stakes(
     block_hash: String,
-    hotkeys: Vec<String>,
-) -> PyResult<Vec<(String, Vec<(String, u64)>)>> {
-    let block_hash = hex::decode(block_hash.trim_start_matches("0x")).expect("Decoding failed");
-    let block_hash = H256::from_slice(&block_hash);
-
-    let concurrency = 32;
-    let api_futures = (0..concurrency).map(|_| get_api()).collect::<Vec<_>>();
-    let api_pool = join_all(api_futures).await.into_iter().collect::<Vec<_>>();
-    let api_pool = unmanaged::Pool::from(api_pool);
-
-    let futures = hotkeys.iter().map(|hotkey| {
-        let hotkey = hotkey.clone();
-        let block_hash = block_hash.clone();
-        let api_pool = api_pool.clone();
-        async move {
-            let api = api_pool.get().await.unwrap();
-            let stakes = query_hotkey_stakes_inner(&api, &block_hash, &hotkey).await;
-            (hotkey, stakes)
-        }
-    });
-    let r = join_all(futures).await;
-    Ok(r)
-}
-
-#[pyfunction]
-fn query_map_pending_emission(block_hash: String) -> PyResult<Vec<(u16, u64)>> {
-    tokio::runtime::Runtime::new()
-        .unwrap()
-        .block_on(query_map_pending_emission_inner(block_hash))
-}
-
-#[pyfunction]
-fn query_subnet_hotkeys(block_hash: String, subnet_id: u16) -> PyResult<Vec<(u16, String)>> {
-    tokio::runtime::Runtime::new()
-        .unwrap()
-        .block_on(query_subnet_hotkeys_inner(block_hash, subnet_id))
-}
-
-#[pyfunction]
-fn query_hotkeys_stakes(
-    block_hash: String,
-    hotkeys: Vec<String>,
 ) -> PyResult<Vec<(String, Vec<(String, u64)>)>> {
     tokio::runtime::Runtime::new()
         .unwrap()
-        .block_on(query_hotkeys_stakes_inner(block_hash, hotkeys))
+        .block_on(query_block_stakes_inner(block_hash))
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn rust_bindings(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(query_map_pending_emission, m)?)?;
-    m.add_function(wrap_pyfunction!(query_subnet_hotkeys, m)?)?;
-    m.add_function(wrap_pyfunction!(query_hotkeys_stakes, m)?)?;
+    m.add_function(wrap_pyfunction!(query_block_stakes, m)?)?;
     Ok(())
 }
