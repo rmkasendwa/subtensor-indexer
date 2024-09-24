894149310dabeea3cbaf79cd17bac331239344d1
hey Michal! <michal@opentensor.dev>
2024-09-25 00:33:15 +0200

    move data fetching to pythonm

--- scraper_service/shovel_daily_balance/main.py
+++ scraper_service/shovel_daily_balance/main.py
@@ -6,7 +6,7 @@ from shared.clickhouse.utils import (
 from shared.shovel_base_class import ShovelBaseClass
 from shared.clickhouse.batch_insert import buffer_insert
 from shared.block_metadata import get_block_metadata
-import rust_bindings
+from shared.substrate import get_substrate_client
 
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s %(process)d %(message)s")
@@ -24,36 +24,44 @@ def do_process_block(n, table_name):
         CREATE TABLE IF NOT EXISTS {table_name} (
             block_number UInt64 CODEC(Delta, ZSTD),
             timestamp DateTime CODEC(Delta, ZSTD),
-            hotkey String CODEC(ZSTD),
-            coldkey String CODEC(ZSTD),
+            address String CODEC(ZSTD),
             balance UInt64 CODEC(Delta, ZSTD)
         ) ENGINE = ReplacingMergeTree()
         PARTITION BY toYYYYMM(timestamp)
-        ORDER BY (coldkey, hotkey, timestamp)
+        ORDER BY (address, timestamp)
         """
         get_clickhouse_client().execute(query)
 
     (block_timestamp, block_hash) = get_block_metadata(n)
-    results = rust_bindings.query_block_stakes(block_hash)
-    print(f"Processing block {n}. Found {len(results)} stake entries")
-    for result in results:
-        hotkey = result[0]
-        coldkey, stake = result[1][0]
+
+    results = fetch_all_free_balances_at_block(block_hash)
+    print(f"Processing block {n}. Found {len(results)} balance entries")
+    for address, balance in results.items():
         buffer_insert(
             table_name,
-            [n, block_timestamp, f"'{hotkey}'", f"'{coldkey}'", stake]
+            [n, block_timestamp, f"'{address}'", balance]
         )
 
 
+def fetch_all_free_balances_at_block(block_hash):
+    substrate = get_substrate_client()
+    balances = substrate.query_map(
+        module='System',
+        storage_function='Account',
+        block_hash=block_hash,
+        page_size=1000
+    )
+
+    free_balances = {}
+    for address in balances:
+        address_id = address[0].value
+        address_info = address[1]
+        free_balances[address_id] = address_info['data']['free']
+
+    return free_balances
+
 def main():
-    # BalanceDailyMapShovel(name="balance_daily_map", skip_interval=7200).start()
-    (block_timestamp, block_hash) = get_block_metadata(213337)
-    print("Start")
-    print(block_hash)
-    results = rust_bindings.query_block_balances(block_hash)
-    print("done!")
-    print(results)
-    print("end")
+    BalanceDailyMapShovel(name="balance_daily_map", skip_interval=7200).start()
 
 
 if __name__ == "__main__":
